0129ebcb46a533b0a24d92cffee79cd72004f9df
maryam2006sadeghi <sadeghimaryam199@gmail.com>
2025-05-19 23:02:14 +0330

    initial commit for inserting data

---
+++ insert_data.sql
@@ -0,0 +1,39 @@
+insert into person values
+(1, 'maryam', '2400351805'),
+(2, 'zhina', '1364014671'),
+(3, 'shirin', '4421400009');
+
+insert into student values
+(40313021, 1, 'mathematical sciences'),
+(40313013, 2, 'marine engineering'),
+(40313019, 3,'CS');
+
+insert into professor values
+(13254, 4, 'computer science', 'professor'),
+(10101, 2, 'marine engineering', 'professor'),
+(20202, 1, 'computer science', 'professor');
+
+insert into person
+values (4, 'parya', '0025935348');
+
+insert into day values
+(1, 'saturday', '1404-03-03'),
+(2, 'sunday', '1404-03-04'),
+(3, 'monday', '1404-03-05');
+
+insert into food values
+(1, 'gheime', 120000),
+(2, 'kabab',130000),
+(3,'kalam polo', 140000);
+
+insert into menu values
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 3),
+(4, 3, 2);
+
+insert into order_list values
+(1, 1, 1, 1, 2),
+(2, 2, 2, 1, 3),
+(3, 3, 3, 2, 4);
+
